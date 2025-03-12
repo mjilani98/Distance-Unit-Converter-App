67a90d15bfd495f2dee26c011b9ca74250e19ecd
mjilani98 <mjilani@emich.edu>
2025-03-12 11:26:47 -0400

    Created Miles to Kilometers intial screen and controller

---
+++ app/src/main/java/com/example/hw3q4/Miles_to_Kilometers.java
@@ -0,0 +1,16 @@
+package com.example.hw3q4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class Miles_to_Kilometers extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //display screen
+        setContentView(R.layout.activity_milekilometers);
+    }
+}
